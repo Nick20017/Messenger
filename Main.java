12ec0e49e067c2e0b7f8f37f4a74b8dd2bcc330e
Nick20017 <mine-modsgames@hotmail.com>
2020-10-15 20:04:45 +0300

    Main.java is modified

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Messenger messenger = new Messenger();
         messenger.Show(args);
+	System.out.println("Shutting down...");
     }
 }
